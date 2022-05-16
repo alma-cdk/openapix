d6adacf7524eb09c979fee12021b3d60929d2683
Ari Palo <ari.palo@almamedia.fi>
2022-05-17 00:15:46 +0300

    feat: update projenrc configuration

--- .projenrc.js
+++ .projenrc.js
@@ -3,32 +3,47 @@ const { awscdk, TextFile, javascript } = require('projen');
 const nodejsVersion = '14.17.6';
 
 const project = new awscdk.AwsCdkConstructLibrary({
+
+  // Metadata
+  stability: 'experimental',
   authorName: 'Alma Media',
   authorOrganization: true,
   authorAddress: 'opensource@almamedia.dev',
-  defaultReleaseBranch: 'main',
-  name: '@almamedia-open-source/cdk-openapix',
-  repositoryUrl: 'https://github.com/almamedia-open-source/cdk-openapix.git',
-  keywords: ['cdk', 'aws-cdk', 'awscdk', 'aws'],
+  name: '@alma-cdk/openapix',
+  description: 'Generate API Gateway definition from OpenAPI schema',
+  repositoryUrl: 'https://github.com/alma-cdk/openapix.git',
+  keywords: ['cdk', 'aws-cdk', 'awscdk', 'aws', 'cloudfront', 'api-gateway', 'rest-api', 'open-api', 'swagger'],
 
   // Publish configuration
   defaultReleaseBranch: 'main',
-  npmAccess: javascript.NpmAccess.PUBLIC,
+  packageManager: javascript.NodePackageManager.NPM,
+  //npmAccess: javascript.NpmAccess.PUBLIC,
 
   // Dependencies
   minNodeVersion: nodejsVersion,
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.24.1',
   constructsVersion: '10.0.0',
   peerDeps: ['constructs', 'aws-cdk-lib'],
   devDeps: ['constructs', 'aws-cdk-lib', '@types/lodash', '@types/js-yaml'],
   bundledDeps: ['change-case', 'lodash', 'js-yaml', 'omit-deep-lodash'],
 
   // Gitignore
-  gitignore: ['.DS_Store'],
+  gitignore: [
+    '.DS_Store',
+    '/examples/**/cdk.context.json',
+    '/examples/**/node_modules',
+    '/examples/**/cdk.out',
+    '/examples/**/.git',
+  ],
+
+
 });
 
 new TextFile(project, '.nvmrc', {
   lines: [nodejsVersion],
 });
+
+project.addPackageIgnore('/examples/');
+
 
 project.synth();
