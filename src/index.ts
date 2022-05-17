ae7921239534cf4719a43d109c5708a6edafaa57
Ari Palo <ari.palo@almamedia.fi>
2022-05-18 02:34:35 +0300

    feat: initial cors implementation

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 export { OpenApi, OpenApiProps } from './api';
 export { OpenApiDefinition as Definition, OpenApiDefinitionProps as DefinitionProps } from './definition';
 export { Integration } from './integrations/base';
-export { LambdaIntegration } from './integrations/lambda';
-export { AwsIntegration } from './integrations/service';
-export { HttpIntegration } from './integrations/http';
-export { MockIntegration } from './integrations/mock';
+export { LambdaIntegration, LambdaIntegrationOptions } from './integrations/lambda';
+export { AwsIntegration, AwsIntegrationProps } from './integrations/service';
+export { HttpIntegration, HttpIntegrationProps } from './integrations/http';
+export { MockIntegration, MockIntegrationProps } from './integrations/mock';
+export { CorsIntegration, CorsIntegrationProps } from './integrations/cors';
 export { Source, SourceDefinition } from './source';
 export { XAmazonIntegration, XAmazonIntegrationResponse } from './x-amazon-integration/base';
---
+++ src/integrations/cors.ts
@@ -0,0 +1,65 @@
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { Construct } from 'constructs';
+import { generateAwsServiceXMockIntegration } from '../x-amazon-integration/mock';
+import { Integration } from './base';
+import { readFileSync } from 'fs';
+const template = readFileSync(__dirname+'/cors.vtl', 'utf-8');
+
+export interface CorsIntegrationProps {
+  validator?: string;
+  headers: string[],
+  origins: string[],
+  methods: string[],
+}
+
+/**
+ *
+ * @see https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
+ * @see https://medium.com/@srikanth650/use-api-gateway-with-mock-integration-to-allow-cors-from-multiple-origins-bdcb431d07d3
+ */
+export class CorsIntegration extends Integration {
+  constructor(scope: Construct, props: CorsIntegrationProps) {
+    super();
+    this.xAmazonApiGatewayRequestValidator = props.validator;
+
+    const origins = this.formatOriginsForTemplate(props.origins);
+    const tmpl = template.replace('__DOMAIN__', origins);
+
+    const methods = this.formatMethodsValue(props.methods);
+    const headers = this.formatHeadersValue(props.headers);
+
+    this.xAmazonIntegration = generateAwsServiceXMockIntegration(scope, {
+      type: apigateway.IntegrationType.MOCK,
+      options: {
+        integrationResponses: [
+          {
+            statusCode: '200',
+            responseParameters: {
+              'method.response.header.Access-Control-Allow-Methods': methods,
+              'method.response.header.Access-Control-Allow-Headers': headers,
+            },
+            responseTemplates: {
+              'application/json': tmpl,
+            },
+          },
+        ],
+      },
+    });
+  }
+
+  private formatOriginsForTemplate(origins: string[]): string {
+    if (origins.length < 1) return '*';
+    return origins.map(o => `"${o}"`).join(', ');
+  }
+
+  private formatMethodsValue(methods: string[]): string {
+    if (methods.length < 1) return '*';
+    return methods.join(',');
+  }
+
+  private formatHeadersValue(headers: string[]): string {
+    return headers.join(',');
+  }
+}
+
+
