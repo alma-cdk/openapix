fffbdc8466e1352ec07d3d771b5d4360c8a9b6b4
Ari Palo <ari.palo@almamedia.fi>
2022-05-20 00:26:42 +0300

    fix: imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 export { OpenApi, OpenApiProps } from './api';
-export { LambdaIntegration, LambdaIntegrationOptions } from './integration/lambda';
+
+
+
+export { Integration, ValidatorConfig } from './integration/base';
 export { AwsIntegration, AwsIntegrationProps } from './integration/aws';
+export { LambdaIntegration, LambdaIntegrationOptions } from './integration/lambda';
 export { HttpIntegration, HttpIntegrationProps } from './integration/http';
 export { MockIntegration, MockIntegrationProps } from './integration/mock';
 export { CorsIntegration, CorsIntegrationProps } from './integration/cors';
+
+
 export { Schema } from './schema';
 //export { XAmazonApigatewayAuthorizer } from './x-amazon-apigateway/authorizer';
 //export { XAmazonApigatewayAuthType } from './x-amazon-apigateway/authtype';
