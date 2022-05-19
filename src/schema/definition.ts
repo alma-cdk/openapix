b7359c779ac1b8bdaa636283a4094bd67db52f99
Ari Palo <ari.palo@almamedia.fi>
2022-05-20 00:24:04 +0300

    fix: security schemes

--- src/schema/definition.ts
+++ src/schema/definition.ts
@@ -1,6 +1,6 @@
 import { HeaderObject, LinkObject, OpenAPI3, ParameterObject, PathItemObject, ReferenceObject, RequestBody, ResponseObject, SchemaObject } from 'openapi-typescript';
-import { SecuritySchemes } from '../security-schemes/interfaces/security-schemes';
-
+import { XAmazonApigatewayAuthorizer } from "../x-amazon-apigateway/authorizer";
+import { XAmazonApigatewayAuthType } from "../x-amazon-apigateway/authtype";
 
 export interface SchemaDefinition extends OpenAPI3 {
   /**
@@ -40,7 +40,7 @@ export interface SchemaDefinition extends OpenAPI3 {
     requestBodies?: Record<string, ReferenceObject | RequestBody>;
     headers?: Record<string, ReferenceObject | HeaderObject>;
     links?: Record<string, ReferenceObject | LinkObject>;
-    securitySchemes?: SecuritySchemes;
+    securitySchemes?: Record<string, SecurityScheme>;
   };
 }
 
@@ -89,4 +89,52 @@ export interface LicenseObject {
 
   /** A URL to the license used for the API. MUST be in the format of a URL. */
   url?: string;
+}
+
+
+
+
+/**
+ * Describes an authorizer defined in `securitySchemes` OpenApi 3 schema.
+ *
+ * @see https://swagger.io/specification/#security-scheme-object
+ */
+export interface SecurityScheme {
+
+  /**
+   * The type of the security scheme.
+   *
+   * OpenAPI v3 allows "apiKey", "http", "oauth2", "openIdConnect" but apparently APIGW only allows "apiKey".
+   *
+   * @example
+   * 'apiKey'
+   */
+  type: 'apiKey';
+
+  /**
+   * A short description for security scheme. CommonMark syntax MAY be used for rich text representation.
+   */
+  description?: string;
+
+  /**
+   * The name of the header, query or cookie parameter to be used.
+   * Applicable for the authorizer of the `apiKey` type only.
+   *
+   * @example
+   * 'Authorization'
+   */
+  name?: string;
+
+  /**
+   * The location of the API key. Valid values are "query", "header" or "cookie".
+   * Applicable for the authorizer of the `apiKey` type only.
+   *
+   * @example
+   * 'header'
+   */
+  in?: 'query'|'header'|'cookie';
+
+  'x-amazon-apigateway-authtype'?: XAmazonApigatewayAuthType;
+
+  'x-amazon-apigateway-authorizer'?: XAmazonApigatewayAuthorizer;
 }
